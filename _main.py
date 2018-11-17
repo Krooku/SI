5916c9557a7120b98027ec636d1cd4133a001013
s426169 <krzkul2@st.amu.edu.pl>
2018-11-17 23:52:39 +0100

    Klasy sapera, bomby, poruszanie się, wczytywanie mapy z pliku do tablicy, testowe sprite'y.

--- _main.py
+++ _main.py
@@ -32,13 +32,12 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (10, 123, 10)
 blue = (0, 0, 100)
-bg_color = black
+bg_color = green
 
 window_width = 992
 window_height = 800
+bokKratki = 40
 FPS = 30
-
-pole = pygame.Rect( 30,30 , window_width - 60, window_height - 60)
 
 class Saper:
     def __init__(self, pozycjax, pozycjay, kierunek):
@@ -47,33 +46,60 @@ class Saper:
         self.kierunek = kierunek #kierunek poruszania sie (0 - dol, 1 - prawo, 2 - gora, 3 - lewo)
         self.saperModel = pygame.image.load("saper.gif")
         self.saperModelRect = self.saperModel.get_rect()
-        self.saperModelRect = self.saperModelRect.move(self.x, self.y)
+        self.saperModelRect.x = self.x*bokKratki
+        self.saperModelRect.y = self.y*bokKratki
 
     def zmienkierunek(self, x):
         if(x == 1): #obroc sie w prawo
             self.saperModel = rotate(self.saperModel, -90)
-            self.kierunek = (self.kierunek+1)%4
+            self.kierunek = (self.kierunek-1)%4
         elif(x == 2):    #obroc sie w lewo
             self.saperModel = rotate(self.saperModel, 90)
-            self.kierunek = (self.kierunek-1)%4
+            self.kierunek = (self.kierunek+1)%4
         elif(x == 0):   #odwroc sie w tyl
             self.saperModel = rotate(self.saperModel, 180)
             self.kierunek = (self.kierunek + 2) % 4
 
-
-    def porusz(self):
+    def kolizja(self,mapa):
         if (self.kierunek == 0):
-            self.saperModelRect.y += 1
-            self.y += 1
+            if (mapa[self.x + 1][self.y] != "0"):
+                return True
+            else:
+                return False
         elif (self.kierunek == 1):
-            self.saperModelRect.x -= 1
-            self.x -= 1
+            if (mapa[self.x][self.y + 1] != "0"):
+                return True
+            else:
+                return False
         elif (self.kierunek == 2):
-            self.saperModelRect.y -= 1
-            self.y -= 1
+            if (mapa[self.x - 1][self.y] != "0"):
+                return True
+            else:
+                return False
         elif (self.kierunek == 3):
-            self.saperModelRect.x += 1
-            self.x += 1
+            if (mapa[self.x][self.y - 1] != "0"):
+                return True
+            else:
+                return False
+
+    def porusz(self, mapa):
+        if not(self.kolizja(mapa)):
+            if (self.kierunek == 0):
+                self.saperModelRect.y += bokKratki
+                self.x += 1
+            elif (self.kierunek == 1):
+                self.saperModelRect.x += bokKratki
+                self.y += 1
+            elif (self.kierunek == 2):
+                self.saperModelRect.y -= bokKratki
+                self.x -= 1
+            elif (self.kierunek == 3):
+                self.saperModelRect.x -= bokKratki
+                self.y -= 1
+        else:
+            print(self.kolizja(mapa))
+
+
 
 class Bomba:
     def __init__(self, pozycjax, pozycjay, uzbrojona):
@@ -98,17 +124,41 @@ def ladujmape(nazwa):   #ladowanie mapy do tablicy
     tablica = [y.split() for y in f.split('\n')]
     return tablica
 
-#inicjalizacja elementow srodowiska
-mapa = ladujmape("mapa1")
-saper = Saper(50,50,0)
-testowaBomba = Bomba(120, 100, True)
+#rysowanie sciany
+def sciana(x,y):
+    scianaModel = pygame.image.load("sciana.gif")
+    scianaRect = pygame.Rect(x*bokKratki, y*bokKratki, bokKratki, bokKratki)
+    gameDisplay.blit(scianaModel, scianaRect)
+
+#rysowanie krzaka
+def krzak(x,y):
+    krzakModel = pygame.image.load("krzak.gif")
+    krzakRect = pygame.Rect(x*bokKratki, y*bokKratki, bokKratki, bokKratki)
+    gameDisplay.blit(krzakModel, krzakRect)
+
+#rysowanie obiektow na mapie
+def rysujobiekty(mapa):
+    posx = 0
+    for a in mapa:
+        posy = 0
+        for b in a:
+            if (b == "1"):
+                sciana(posy, posx)
+            elif (b == "4"):
+                krzak(posy, posx)
+            posy += 1
+        posx += 1
+
 
 gameDisplay = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Saper')
-
+clock = pygame.time.Clock()
 gameExit = False
 
-clock = pygame.time.Clock()
+#inicjalizacja elementow srodowiska
+mapa = ladujmape("mapa1")
+testowaBomba = Bomba(120, 100, True)
+saper = Saper(2, 2, 0)
 
 while not gameExit: #game_loop
     for event in pygame.event.get(): #event_loop
@@ -120,7 +170,7 @@ while not gameExit: #game_loop
             if event.key == pygame.K_DOWN:
                 saper.zmienkierunek(0)
             if event.key == pygame.K_UP:
-                saper.porusz()
+                saper.porusz(mapa)
             if event.key == pygame.K_RIGHT:
                 saper.zmienkierunek(1)
             if event.key == pygame.K_LEFT:
@@ -131,12 +181,11 @@ while not gameExit: #game_loop
     #kwadrat.x += math.sin(clock.tick(FPS))
     #kwadrat.y += math.cos(clock.tick(FPS))
     gameDisplay.fill(bg_color)
-    pygame.draw.rect(gameDisplay, green, pole)
+    rysujobiekty(mapa)
     gameDisplay.blit(saper.saperModel, saper.saperModelRect)
     gameDisplay.blit(testowaBomba.bombaModel, testowaBomba.bombaModelRect)
     tekst(saper.kierunek)
     pygame.display.update()
-
     clock.tick(FPS)
 pygame.quit()
 quit()
