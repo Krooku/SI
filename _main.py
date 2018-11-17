3a283204829e72e0fa8157355adbed6a313fc55d
s426169 <krzkul2@st.amu.edu.pl>
2018-11-17 21:10:46 +0100

    Klasy sapera, bomby, poruszanie się, wczytywanie mapy z pliku do tablicy, testowe sprite'y.

--- _main.py
+++ _main.py
@@ -2,34 +2,108 @@ import pygame
 import math
 import sys
 
+
+# do zrobienia
+
+# MAPA
+# tablica X * Y
+#
+
+# SAPER:
+# wybiera_kierunek do poruszania sie
+# Sprawdza czy przed nim znajduje sie mina
+# Jesli tak, probuje ja rozbroic
+# Sprawdza, czy w poblizu znajduje sie niesprawdzone miejsce, kieruje sie tam
+# omija sciany, oraz inne przeszkody
+# Oznacza przebyta droge jako bezpieczna
+
+# MINA:
+# kazda na poczatku jest aktywna (oznaczona cyfra 2)
+# rozbrojona zostaje oznaczona cyfra 3 oraz inna tekstura
+
+
+# Rozminowanie konczy sie po rozbrojeniu wszystkich min przez sapera
+from pygame.transform import rotate
+
 pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-
-bg_color = blue
-
+green = (10, 123, 10)
+blue = (0, 0, 100)
+bg_color = black
 
 window_width = 992
 window_height = 800
 FPS = 30
 
-width = 50
-height = 50
-pos_x = window_width / 2 - width / 2
-pos_y = window_height / 2 + height / 2
-dir = 0
-speed = 10
+pole = pygame.Rect( 30,30 , window_width - 60, window_height - 60)
 
-kwadrat = pygame.Rect(pos_x, pos_y, width, height)
+class Saper:
+    def __init__(self, pozycjax, pozycjay, kierunek):
+        self.x = pozycjax
+        self.y = pozycjay
+        self.kierunek = kierunek #kierunek poruszania sie (0 - dol, 1 - prawo, 2 - gora, 3 - lewo)
+        self.saperModel = pygame.image.load("saper.gif")
+        self.saperModelRect = self.saperModel.get_rect()
+        self.saperModelRect = self.saperModelRect.move(self.x, self.y)
+
+    def zmienkierunek(self, x):
+        if(x == 1): #obroc sie w prawo
+            self.saperModel = rotate(self.saperModel, -90)
+            self.kierunek = (self.kierunek+1)%4
+        elif(x == 2):    #obroc sie w lewo
+            self.saperModel = rotate(self.saperModel, 90)
+            self.kierunek = (self.kierunek-1)%4
+        elif(x == 0):   #odwroc sie w tyl
+            self.saperModel = rotate(self.saperModel, 180)
+            self.kierunek = (self.kierunek + 2) % 4
 
 
+    def porusz(self):
+        if (self.kierunek == 0):
+            self.saperModelRect.y += 1
+            self.y += 1
+        elif (self.kierunek == 1):
+            self.saperModelRect.x -= 1
+            self.x -= 1
+        elif (self.kierunek == 2):
+            self.saperModelRect.y -= 1
+            self.y -= 1
+        elif (self.kierunek == 3):
+            self.saperModelRect.x += 1
+            self.x += 1
+
+class Bomba:
+    def __init__(self, pozycjax, pozycjay, uzbrojona):
+        self.uzbrojona = uzbrojona
+        self.x = pozycjax
+        self.y = pozycjay
+        self.bombaModel = pygame.image.load("bomba.gif")
+        self.bombaModelRect = self.bombaModel.get_rect()
+        self.bombaModelRect = self.bombaModelRect.move(self.x, self.y)
+
+    def rozbroj(self):
+        self.uzbrojona = False
+        self.bombaModel = pygame.image.load("bombarozbrojona.gif")
+
+def tekst(x):   #informacje na ekranie
+   font=pygame.font.SysFont("monospace", 15)
+   napis=font.render("kierunek:"+str(x) + " pozycja: " + str(saper.x) + " " + str(saper.y) , 1,(255,255,255))
+   gameDisplay.blit(napis, (0, 0))
+
+def ladujmape(nazwa):   #ladowanie mapy do tablicy
+    f = open(nazwa).read()
+    tablica = [y.split() for y in f.split('\n')]
+    return tablica
+
+#inicjalizacja elementow srodowiska
+mapa = ladujmape("mapa1")
+saper = Saper(50,50,0)
+testowaBomba = Bomba(120, 100, True)
 
 gameDisplay = pygame.display.set_mode((window_width, window_height))
-
 pygame.display.set_caption('Saper')
 
 gameExit = False
@@ -44,33 +118,23 @@ while not gameExit: #game_loop
             if event.key == pygame.K_ESCAPE:
                 gameExit = True
             if event.key == pygame.K_DOWN:
-                dir = 3
+                saper.zmienkierunek(0)
             if event.key == pygame.K_UP:
-                dir = 1
+                saper.porusz()
             if event.key == pygame.K_RIGHT:
-                dir = 2
+                saper.zmienkierunek(1)
             if event.key == pygame.K_LEFT:
-                dir = 4
+                saper.zmienkierunek(2)
+            if event.key == pygame.K_k: #test rozbrojenia bomby
+                testowaBomba.rozbroj()
 
     #kwadrat.x += math.sin(clock.tick(FPS))
     #kwadrat.y += math.cos(clock.tick(FPS))
-
-    if(dir == 1): #north
-        kwadrat.y -= speed
-        pos_y -= speed
-    elif(dir == 2): #east
-        kwadrat.x += speed
-        pos_x += speed
-    elif(dir == 3): #south
-        kwadrat.y += speed
-        pos_y += speed
-    elif(dir == 4): #west
-        kwadrat.x -= speed
-        pos_x -= speed
-
     gameDisplay.fill(bg_color)
-    pygame.draw.rect(gameDisplay, green, kwadrat)
-
+    pygame.draw.rect(gameDisplay, green, pole)
+    gameDisplay.blit(saper.saperModel, saper.saperModelRect)
+    gameDisplay.blit(testowaBomba.bombaModel, testowaBomba.bombaModelRect)
+    tekst(saper.kierunek)
     pygame.display.update()
 
     clock.tick(FPS)
